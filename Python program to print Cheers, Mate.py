1b2ad8b7e561f691ed59e9b4bd4cffa33ce96941
Shashank-ganiger <149599674+Shashank-ganiger@users.noreply.github.com>
2023-11-06 22:23:31 +0530

    Add files via upload

---
+++ Python program to print Cheers, Mate.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+
+# In[2]:
+
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!") 
+
+
+# In[3]:
+
+
+"""""
+This is comment
+written in
+more than just one line
+"""""
+print("Hello, World!")
+
+
+# In[4]:
+
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+
+# In[5]:
+
+
+x = 4
+x = "Shashank"
+print(x)
+
+
+# In[6]:
+
+
+x = 4 #x is of type int
+x = "Shashank" #x is of type String
+print(x)
+
+
+# In[7]:
+
+
+x = str(A)
+
+
+# In[10]:
+
+
+x = str(4) 
+y = int(4)
+z = float(4)
+print(x)
+print(y)
+print(z)
+
+
+# In[11]:
+
+
+x = str(4) #x will be 4
+y = int(4) #y will be 4
+z = float(4) #z will be 4
+print(x)
+print(y)
+print(z)
+
+
+# In[12]:
+
+
+x = 5
+y = "john"
+print(type(x))
+print(type(y))
+
+
+# In[13]:
+
+
+x = "john"
+print(x)
+X = 'john'
+print(x)
+
+
+# In[14]:
+
+
+a = 4
+A = "Sally"
+print(a)
+print(A)
+
+
+# In[15]:
+
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+
+
+# In[16]:
+
+
+2myvar = "John"
+my-var = "John"
+my var = "John"
+print(2myvar)
+print(my-var)
+print(my var)
+
+
+# In[ ]:
+
+
+
+
