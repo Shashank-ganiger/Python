4dbc34c76163e069ffacfb33b395b3edad961eb6
Shashank-ganiger <149599674+Shashank-ganiger@users.noreply.github.com>
2023-11-06 20:56:03 +0530

    Add files via upload

---
+++ Hello world.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello, World!")
+
+
+# In[3]:
+
+
+print("Hello, World!")
+        
+
+
+# In[4]:
+
+
+if 5 > 2:
+  print("Five is greater than two!")
+
+
+# In[5]:
+
+
+if 5 > 2:
+print("Five is greater than two!")
+
+
+# In[6]:
+
+
+x = 5
+y = "Hello, World!"
+
+print(x)
+print(y)
+
+
+# In[7]:
+
+
+#This is a comment.
+print("Hello, World!")
+
+
+# In[8]:
+
+
+print("Hello, World!") 
+#This is a comment.
+
+
+# In[ ]:
+
+
+
+
